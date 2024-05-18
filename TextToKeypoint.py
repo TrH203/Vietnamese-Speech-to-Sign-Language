9b2c601a19e1127b122030719598e206dcecd420
hoangtrungkien2109 <hoangtrungkien2109@gmail.com>
2024-05-18 16:09:00 +0700

    Refactor: rename output folder to keypointdata Add: lstm test model (Unrunnable)

---
+++ TextToKeypoint.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers import LSTM, Dense, Embedding, Bidirectional, Dropout, Input
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+dataset_label = pd.read_csv('hand1.csv', header=None).values
+
+# Define the dataset
+text = [
+    "Hello Guys",
+    "How are you"
+]
+labels = dataset_label
+
+# Tokenize and preprocess the text data
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(text)
+sequences = tokenizer.texts_to_sequences(text)
+vocab_size = len(tokenizer.word_index) + 1
+
+num_encoder_tokens = vocab_size
+batch_size = 1
+num_decoder_tokens = 63
+latent_dim = 128
+max_sequence_length = 10  # maximum length of input sequences
+epochs = 2
+
+padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length)
+
+encoder_input_data = padded_sequences
+decoder_input_data = labels[:-1]
+decoder_target_data = labels[1:]
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='linear')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.compile(optimizer='rmsprop', loss='mse')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
